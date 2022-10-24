4e1624413b85563b81d1b71dba22003cedc1249a
Diegoeraso1239 <53222685+Diegoeraso1239@users.noreply.github.com>
2022-10-24 10:42:21 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -8,13 +8,43 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
+import re
+def format_date(str_date):
+    d = re.search(r'(^\d+)\/(\d+)\/(\d+)', str_date, re.IGNORECASE)
+    day = d.group(1)
+    month = d.group(2)
+    year = d.group(3)
+    if len(day)>2:
+        date = year + '/' + month + '/' + day
+        return date
+    else:
+        date = day + '/' + month + '/' + year
+        return date
 
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df= df[df.columns[1:]]
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
 
-    #
-    # Inserte su código aquí
-    #
-
+    for i in df.columns:
+        try:
+            df[i] = df[i].str.lower()
+        except:
+            pass
+    df.idea_negocio = df.idea_negocio.map(lambda x: re.sub(r'_|-', ' ', str(x)))
+    df.barrio = df.barrio.map(lambda x: re.sub(r'-| ', '_', str(x)))
+    # df.barrio = df.barrio.map(lambda x: re.sub(r'\s', '_', str(x)))
+    df.fecha_de_beneficio = df.fecha_de_beneficio.apply(format_date)
+    df.monto_del_credito = df.monto_del_credito.map(lambda x: re.sub(r'\$', '', str(x)))
+    df.monto_del_credito = df.monto_del_credito.map(lambda x: float(x.replace(',', '')))
+    df['línea_credito'] = df['línea_credito'].map(lambda x: re.sub(r'_|-', ' ', str(x)))
+    for i in df.columns:
+            try:
+                df[i] = df[i].str.strip()
+            except:
+                pass
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
     return df
